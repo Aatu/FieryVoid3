836ecf30d66330150d651971cd718ddd24f4a29b
Aatu <aatu.riuttamaki@gmail.com>
2020-02-09 19:49:33 +0200

    revert server url

--- app/src/client/config.js
+++ app/src/client/config.js
@@ -1,6 +1,6 @@
-const SERVER_URL = "http://localhost:4000";
-//const SERVER_URL = "http://95.217.10.191:4000";
-const SERVER_WEBSOCKET_URL = "ws://localhost:4000/game/";
-//const SERVER_WEBSOCKET_URL = "ws://95.217.10.191:4000/game/";
+//const SERVER_URL = "http://localhost:4000";
+const SERVER_URL = "http://95.217.10.191:4000";
+//const SERVER_WEBSOCKET_URL = "ws://localhost:4000/game/";
+const SERVER_WEBSOCKET_URL = "ws://95.217.10.191:4000/game/";
 
 export { SERVER_URL, SERVER_WEBSOCKET_URL };
