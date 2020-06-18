70a992a120e07792e27ea865c4bc1e04cd7cd519
Aatu <aatu.riuttamaki@gmail.com>
2020-06-18 18:43:58 +0300

    add build

---
+++ app/build/precache-manifest.b5c6ceda0aa3545870c75a6f62e9c2be.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e139e5dea483efb9531a227e3ac4113",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e5429ec6ec821676f05",
+    "url": "/static/js/2.d545c0b9.chunk.js"
+  },
+  {
+    "revision": "9e2069e239c451d58b21",
+    "url": "/static/js/main.f67988ea.chunk.js"
+  },
+  {
+    "revision": "ac8ebff2a6da512025d2",
+    "url": "/static/js/runtime-main.177e5dfc.js"
+  }
+]);
